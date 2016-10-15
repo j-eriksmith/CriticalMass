6788b5853970fccd5a91933f45c0a5ff481d3b3e
kvax12v <kaylab@dhcp-v027-101.mobile.uci.edu>
2016-10-15 16:09:22 -0700

    Uploaded ControlScript

--- TopDown/Assets/ControlScript.cs
+++ TopDown/Assets/ControlScript.cs
@@ -10,7 +10,7 @@ public class ControlScript : MonoBehaviour {
 	Rigidbody2D rb;
 	public float maxSpeed= 10f;
 	Vector2 directionCannon;
-
+	public int coolDown = 0;
 
 
 	void Start () 
@@ -22,7 +22,8 @@ public class ControlScript : MonoBehaviour {
 
 	void Update() 
 	{
-		if (Input.GetKeyDown ("d")) {
+		coolDown++;
+		if (Input.GetKeyDown ("d") && coolDown >= 30) {
 			Debug.Log ("Pew pew");
 			GameObject ballInstance;
 			ballInstance = Instantiate (Cannon1, spawnPoint.transform.position, spawnPoint.rotation) as GameObject;
@@ -30,6 +31,7 @@ public class ControlScript : MonoBehaviour {
 			directionCannon = rb.velocity + ((Vector2)transform.up * maxSpeed);
 			rb.AddForce (-directionCannon * force);
 			rb.velocity = transform.forward * maxSpeed;
+			coolDown = 0;
 		}
 		}
 
