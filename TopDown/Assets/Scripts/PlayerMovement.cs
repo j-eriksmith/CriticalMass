3ea3da81c893ec81b5072a6359825417107741e0
kvax12v <kaylab@dhcp-v027-101.mobile.uci.edu>
2016-10-14 23:42:48 -0700

    Updated AddForce Controller

--- TopDown/Assets/Scripts/PlayerMovement.cs
+++ TopDown/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,19 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	public Transform target;
+	
 	Rigidbody2D rigid;
+	public float thrust = 10;
 	float leftDx;
 	float leftDy;
 	float playerSpeedX = 500;
 	float playerSpeedY = 500;
+	Vector3 vec;
 
 	// Use this for initialization
 	void Start () {
 
-
+		rigid = GetComponent<Rigidbody2D> ();
 	
 	}
 	
@@ -23,14 +25,23 @@ public class PlayerMovement : MonoBehaviour {
 
 
 
-		float leftDx = Input.GetAxis ("LeftJoystickX");
-		float leftDy = Input.GetAxis ("LeftJoystickY");
+		float leftDx = Input.GetAxis ("Horizontal");
+		float leftDy = Input.GetAxis ("Vertical");
 
-		//float leftDx = Input.GetAxis ("Horizontal");
-		//float leftDy = Input.GetAxis("Vertical");
+		//float leftDx = Input.GetAxis ("LeftJoystickX");
+		//float leftDy = Input.GetAxis("LeftJoystickX");
 
-		GetComponent<Rigidbody2D> ().velocity = new Vector2 (leftDx * playerSpeedX, leftDy * playerSpeedY) * Time.deltaTime;
+		//rigid.velocity = new Vector2 (leftDx * playerSpeedX, leftDy * playerSpeedY) * Time.deltaTime;
 
+		vec = new Vector3(leftDx, leftDy, 0);
+
+		if (Input.GetAxisRaw ("Horizontal") != 0)
+			rigid.AddForce (vec * thrust);
+
+		if (Input.GetAxisRaw ("Vertical") != 0)
+			rigid.AddForce (vec * thrust);
+			
+		
 		
 
 	}
