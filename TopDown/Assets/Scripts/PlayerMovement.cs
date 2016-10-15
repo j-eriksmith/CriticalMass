9ab271bfc1bfdd18a3f8ceca28d28fa9bc8411fb
kvax12v <kaylab@dhcp-v027-101.mobile.uci.edu>
2016-10-14 22:57:56 -0700

    Updated PlayerMovement

---
+++ TopDown/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public Transform target;
+	Rigidbody2D rigid;
+	float dx;
+	float dy;
+	float playerSpeedX = 500;
+	float playerSpeedY = 500;
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+
+	void Update () {
+		
+		
+		float dx = Input.GetAxis ("Horizontal");
+		float dy = Input.GetAxis ("Vertical");
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (dx * playerSpeedX, dy * playerSpeedY) * Time.deltaTime;
+
+
+	}
+}
