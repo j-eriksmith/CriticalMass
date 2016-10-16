e739524427e8c42f83af3731810eec1b890b43a7
Full name <email>
2016-10-16 14:50:43 -0700

    stars have idle animations

--- TopDown/Assets/Blackhole.cs
+++ TopDown/Assets/Blackhole.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Blackhole : MonoBehaviour {
+public class Blackhole : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float spinRate;
 
 	// Use this for initialization
 	void Start () {
-	
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.angularVelocity = spinRate;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 
     void OnTriggerEnter2D(Collider2D other)
