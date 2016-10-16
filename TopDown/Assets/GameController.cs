b046e85260c9904e606804beeccf6ca817a22dd2
Kasen Chaque <kchaque@uci.edu>
2016-10-16 13:28:32 -0700

    Added GameController

---
+++ TopDown/Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+    bool end = false;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (end == true)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                ReloadCurrentScene();
+            }
+        }
+	}
+
+    public void GameOver(string player)
+    {
+        end = true;
+    }
+
+    void ReloadCurrentScene()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
--- TopDown/Assets/Planet_Script.cs
+++ TopDown/Assets/Planet_Script.cs
@@ -5,13 +5,17 @@ public class Planet_Script : MonoBehaviour {
 
     public float growthFactor;
     float size = .3f;
+    public string playerNumber;
+
+    GameObject cameraObject;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
-	    
-	}
+
+        cameraObject = GameObject.Find("Main Camera");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,6 +28,8 @@ public class Planet_Script : MonoBehaviour {
         {
             this.transform.localScale = new Vector3(.3f, .3f, 0);
             size = .3f;
+
+            cameraObject.GetComponent<GameController>().GameOver(playerNumber);
         }
             
 
