5d1ec3ba7aa7e1007971868e856238365a1d88d7
Full name <email>
2016-10-16 20:40:37 -0700

    Final push

--- TopDown/Assets/MenuScreen.cs
+++ TopDown/Assets/MenuScreen.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class MenuScreen : MonoBehaviour {
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel("UIStart");
+        }
+    }
 	public void ChangeScene(string sceneName){
 		Application.LoadLevel(sceneName);
-
 	}
 }
